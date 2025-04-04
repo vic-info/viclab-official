d0fc18c9d439cbc2b425fb37dd5a9a4ae3e639fd
Vic <vic.hsun.llc@gmail.com>
2025-04-04 14:29:44 -0700

    use github workflow

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  devIndicators: false,
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/viclab-official',
-  assetPrefix: '/viclab-official/',
 };
 
 export default nextConfig;
